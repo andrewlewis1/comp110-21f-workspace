a9424e08de95bfe745e15379a996d49f16e105c0
Andrew Lewis <andrewja@email.unc.edu>
2021-10-07 16:19:51 -0400

    Progress on Exercise 5

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,37 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730384041"
 
-# Define your functions below
+
+def only_evens(x: list[int]) -> list[int]:
+    i: int = 0
+    result: list[int] = []
+    while i < len(x):
+        if x[i] % 2 == 0:
+            result.append(x[i])
+        i += 1
+    return result
+
+
+def sub(a_list: list[int], start: int, end: int) -> list[int]:
+    if start < 0:
+        start = 0
+    if end > len(a_list):
+        end = len(a_list)
+    if len(a_list) == 0 or start > len(a_list) or end <= 0:
+        return []
+    start = a_list[start]
+    end = a_list[end - 1]
+    a_list.clear()
+    a_list.append(start)
+    a_list.append(end)
+    return a_list
+
+
+def concat(x: list[int], y: list[int]) -> list[int]:
+    List: list[int] = x
+    i: int = 0
+    while i < len(y):
+        List.append(y[i])
+        i += 1
+    return List
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,54 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730384041"
+from exercises.ex05.utils import only_evens, sub, concat
+
+
+def test_only_evens_all_odds() -> None:
+    x: list[int] = [1, 5, 7, 13]
+    assert only_evens(x) == []
+
+
+def test_only_evens_all_evens() -> None:
+    x: list[int] = [2, 4, 8]
+    assert only_evens(x) == [2, 4, 8]
+
+
+def test_only_evens_odds_and_evens() -> None:
+    x: list[int] = [1, 4, 3, 6, 7]
+    assert only_evens(x) == [4, 6]
+
+
+def test_sub_negative_start_zero_end() -> None:
+    x: list[int] = [1, 2, 3, 4, 5]
+    assert sub(x, -2, 0) == []
+
+
+def test_sub_negative_start_greater_end() -> None:
+    x: list[int] = [1, 2, 3, 4, 5]
+    assert sub(x, -1, 7) == [1, 5]
+
+
+def test_sub_normal() -> None:
+    x: list[int] = [1, 2, 3, 4, 5]
+    assert sub(x, 2, 4) == [3, 4]
+
+
+def test_concat_both_empty() -> None:
+    x: list[int] = []
+    y: list[int] = []
+    assert concat(x, y) == []
+
+
+def test_concat_empty_y() -> None:
+    x: list[int] = [10, 20, 30, 40]
+    y: list[int] = []
+    assert concat(x, y) == x
+
+
+def test_concat_normal_lists() -> None:
+    x: list[int] = [10, 20, 30, 40]
+    y: list[int] = [50, 60, 70]
+    assert concat(x, y) == [10, 20, 30, 40, 50, 60, 70]
