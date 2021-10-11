3b68c6f9ab070f79fd4a5ff367c3f1dbbaf7cd58
Andrew Lewis <andrewja@email.unc.edu>
2021-10-11 16:04:31 -0400

    Progress on Exercise 5

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -4,6 +4,7 @@ __author__ = "730384041"
 
 
 def only_evens(x: list[int]) -> list[int]:
+    """Given a list of ints, only_evens should return a list containing only the elements of the input list that were even."""
     i: int = 0
     result: list[int] = []
     while i < len(x):
@@ -14,11 +15,12 @@ def only_evens(x: list[int]) -> list[int]:
 
 
 def sub(a_list: list[int], start: int, end: int) -> list[int]:
+    """Given a list of ints, a start index, and an end index (not inclusive), sub should generate a List which is a subset of the given list, between the specified start index and the end index - 1."""
     if start < 0:
         start = 0
     if end > len(a_list):
         end = len(a_list)
-    if len(a_list) == 0 or start > len(a_list) or end <= 0:
+    if len(a_list) == 0 or start >= len(a_list) or end <= 0:
         return []
     start = a_list[start]
     end = a_list[end - 1]
@@ -29,6 +31,7 @@ def sub(a_list: list[int], start: int, end: int) -> list[int]:
 
 
 def concat(x: list[int], y: list[int]) -> list[int]:
+    """Given two Lists of ints, concat should generate a new List which contains all of the elements of the first list followed by all of the elements of the second list."""
     List: list[int] = x
     i: int = 0
     while i < len(y):
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -7,48 +7,57 @@ from exercises.ex05.utils import only_evens, sub, concat
 
 
 def test_only_evens_all_odds() -> None:
+    """This tests only_evens with a list of all odd numbers."""
     x: list[int] = [1, 5, 7, 13]
     assert only_evens(x) == []
 
 
 def test_only_evens_all_evens() -> None:
+    """This tests only_evens with a list of all even numbers."""
     x: list[int] = [2, 4, 8]
     assert only_evens(x) == [2, 4, 8]
 
 
 def test_only_evens_odds_and_evens() -> None:
+    """This tests only_evens with a list of a mixture of even and odd numbers."""
     x: list[int] = [1, 4, 3, 6, 7]
     assert only_evens(x) == [4, 6]
 
 
 def test_sub_negative_start_zero_end() -> None:
+    """This tests sub with a negative start value and an end value of zero."""
     x: list[int] = [1, 2, 3, 4, 5]
     assert sub(x, -2, 0) == []
 
 
 def test_sub_negative_start_greater_end() -> None:
+    """This tests sub with a negative start value and an end value greater than the length of the list."""
     x: list[int] = [1, 2, 3, 4, 5]
     assert sub(x, -1, 7) == [1, 5]
 
 
 def test_sub_normal() -> None:
+    """This tests sub with a start value less than the end value, which are both within the indexes of the list."""
     x: list[int] = [1, 2, 3, 4, 5]
     assert sub(x, 2, 4) == [3, 4]
 
 
 def test_concat_both_empty() -> None:
+    """This tests concat with two empty lists."""
     x: list[int] = []
     y: list[int] = []
     assert concat(x, y) == []
 
 
 def test_concat_empty_y() -> None:
+    """This tests concat with an empty second list."""
     x: list[int] = [10, 20, 30, 40]
     y: list[int] = []
     assert concat(x, y) == x
 
 
 def test_concat_normal_lists() -> None:
+    """This tests concat with two nonempty lists."""
     x: list[int] = [10, 20, 30, 40]
     y: list[int] = [50, 60, 70]
     assert concat(x, y) == [10, 20, 30, 40, 50, 60, 70]
