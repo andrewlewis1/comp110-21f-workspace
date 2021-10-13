dda325a2086c441df4bd06f95a301c01de73bc7d
Andrew Lewis <andrewja@email.unc.edu>
2021-10-13 17:17:10 -0400

    Progress on Exercise 6

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,36 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730384041"
 
-# Define your functions below
+
+def invert(x: dict[str, str]) -> dict[str, str]:
+    """Given a dictionary of [str, str], invert should return a dict[str, str] that inverts the keys and the values."""
+    y: dict[str, str] = {}
+    for key, value in x.items():
+        if value in y.keys():
+            raise KeyError("More than one of the same value cannot be inverted into keys!")
+        else:
+            y[value] = key
+    return y
+
+
+def favorite_color(x: dict[str, str]) -> str:
+    """favorite_color has one parameter, of type dict[str, str] of names and favorite colors and returns a str which is the color that appears most frequently."""
+    y: dict[str, int] = {}
+    for key, value in x.items():
+        if value in y:
+            y[value] += 1
+        else:
+            y[value] = 1
+    return max(y, key=y.get)
+
+
+def count(x: list[str]) -> dict[str, int]:
+    """Given a list[str], this function will produce a dict[str, int] where each key is a unique value in the given list and each value associated is the count of the number of times that value appeared in the input list."""
+    y: dict[str, int] = {}
+    for key in x:
+        if key in y:
+            y[key] += 1
+        else:
+            y[key] = 1
+    return y
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,58 @@
 """Unit tests for dictionary functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+__author__ = "730384041"
+from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+
+def test_invert_empty_dict() -> None:
+    """Tests invert with an input of an empty dictionary."""
+    x: dict[str, str] = {}
+    assert invert(x) == {}
+
+
+def test_invert_one_item() -> None:
+    """Tests invert with an input of a dictionary with one key and one value."""
+    x: dict[str, str] = {"Andrew": "Lewis"}
+    assert invert(x) == {"Lewis": "Andrew"}
+
+
+def test_invert_three_items() -> None:
+    """Tests invert with an input of a dictionary with three keys and three values."""
+    x: dict[str, str] = {"Andrew": "Lewis", "John": "Doe", "Jane": "Smith"}
+    assert invert(x) == {"Lewis": "Andrew", "Doe": "John", "Smith": "Jane"}
+
+
+def test_favorite_color_all_appear_once() -> None:
+    """Tests favorite_color with an input of a dictionary with three different favorite colors that all appear once."""
+    x: dict[str, str] = {"Andrew": "red", "James": "blue", "Lewis": "green"}
+    assert favorite_color(x) == "red"
+
+
+def test_favorite_color_two_colors() -> None:
+    """Tests favorite_color with an input of a dictionary with two different favorite colors, one of which appears twice."""
+    x: dict[str, str] = {"Andrew": "blue", "James": "yellow", "Lewis": "blue"}
+    assert favorite_color(x) == "blue"
+
+
+def test_favorite_color_three_colors() -> None:
+    """Tests favorite_color with an input of a dictionary with three different favorite colors, one of which appears twice."""
+    x: dict[str, str] = {"Andrew": "orange", "James": "purple", "Lewis": "red", "John": "purple"}
+    assert favorite_color(x) == "purple"
+
+
+def test_count_empty_list() -> None:
+    """Tests count with an input of an empty list."""
+    x: list[str] = []
+    assert count(x) == {}
+
+
+def test_count_three_items() -> None:
+    """Tests count with an input of a list with two different items, one of which appears twice."""
+    x: list[str] = ["Andrew", "James", "Andrew"]
+    assert count(x) == {"Andrew": 2, "James": 1}
+
+
+def test_count_five_items() -> None:
+    """Tests count with an input of a list with three different items, one of which appears once and one of which appears thrice."""
+    x: list[str] = ["Andrew", "James", "Andrew", "Lewis", "Andrew", "James"]
+    assert count(x) == {"Andrew": 3, "James": 2, "Lewis": 1}
