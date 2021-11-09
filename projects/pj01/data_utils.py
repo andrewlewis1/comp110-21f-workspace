0523e3195788d322cff60083cfadd0947778d954
Andrew Lewis <andrewja@email.unc.edu>
2021-11-08 19:46:21 -0500

    Progress on Project 1

---
+++ projects/pj01/data_utils.py
@@ -0,0 +1,109 @@
+"""Utility functions."""
+
+__author__ = "730384041"
+from csv import DictReader
+
+
+# Define your functions below
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read the rows of a csv into a 'table'."""
+    result: list[dict[str, str]] = []
+    file_handle = open(filename, "r", encoding="utf8")
+    csv_reader = DictReader(file_handle)
+    for row in csv_reader:
+        result.append(row)
+    file_handle.close()
+
+    return result
+
+
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
+    """Produce a list[str] of all values in a single column."""
+    result: list[str] = []
+    for row in table:
+        item: str = row[column]
+        result.append(item)
+
+    return result
+
+
+def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Transform a row-oriented table into a column-oriented table."""
+    result: dict[str, list[str]] = {}
+    first_row: dict[str, str] = row_table[0]
+    for column in first_row:
+        result[column] = column_values(row_table, column)
+
+    return result
+
+
+def head(column_table: dict[str, list[str]], n: int) -> dict[str, list[str]]:
+    """Produce a new column-based table with only the first n rows of data for each column."""
+    result: dict[str, list[str]] = {}
+    for column in column_table:
+        if n >= len(column_table[column]):
+            return column_table
+        column_data: list[str] = []
+        i: int = 0
+        while i < n:    
+            column_data.append(column_table[column][i])
+            i += 1
+        result[column] = column_data
+
+    return result
+
+
+def select(column_table: dict[str, list[str]], names: list[str]) -> dict[str, list[str]]:
+    """Produce a new column-based table with only a specific subset of the original columns."""
+    result: dict[str, list[str]] = {}
+    for column in names:
+        result[column] = column_table[column]
+    
+    return result
+
+
+def concat(first_table: dict[str, list[str]], second_table: dict[str, list[str]]) -> dict[str, list[str]]:
+    """Produce a new column-based table with two column-based tables combined."""
+    result: dict[str, list[str]] = {}
+    for column in first_table:
+        result[column] = first_table[column]
+    for column in second_table:
+        if column in result.keys():
+            result[column] += second_table[column]
+        else:
+            result[column] = second_table[column]
+
+    return result
+
+
+def count(items: list[str]) -> dict[str, int]:
+    """Given a list[str], this function will produce a dict[str, int] where each key is a unique value in the given list and each value associated is the count of the number of times that value appeared in the input list."""
+    result: dict[str, int] = {}
+    for item in items:
+        if item in result:
+            result[item] += 1
+        else:
+            result[item] = 1
+
+    return result
+
+
+def combinations(column_table: dict[str, list[str]], column1: str, column2: str) -> dict[str, int]:
+    """Given a column-oriented data table and two of the columns in the table, combinations will return all combinations of values for a particular row in both columns and their occurences."""
+    result: dict[str, int] = {}
+    list1: list[str] = column_table[column1]
+    list2: list[str] = column_table[column2]
+    combo_list: list[str] = []
+    i: int = 0
+    while i < len(list1) and i < len(list2):
+        combo_list.append(f"{list1[i]} and {list2[i]}/7")
+        i += 1
+    i = 0
+    while i < len(combo_list):
+        if combo_list[i] in result:
+            result[combo_list[i]] += 1
+        else:
+            result[combo_list[i]] = 1
+        i += 1
+
+    return result
